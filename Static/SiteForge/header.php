516424404edd981928272c1d72b0d81afb64e35f
Juraj Besta <juraj.besta@gmail.com>
2023-07-06 17:45:13 +0200

    Delete old includes

--- Static/SiteForge/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <div class="container-fluid" id="top-row">
-        <div id="top-intro-txt">
-            <!-- some intro in header -->
-            Light-weight rapid application development framework for PHP 8. Easy to use and completely free.
-        </div>
-    </div>
-</header>
