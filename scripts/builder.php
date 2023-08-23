6ae62aaaceb26b753496ab11b4ef96f4c6fe57c7
Dusan <dusan.daniska@wai.sk>
2023-08-23 09:21:02 +0200

    uvodne instrukcie pre builder

--- scripts/builder.php
+++ scripts/builder.php
@@ -1,3 +1,9 @@
 <?php
 
+/*
+  - recursively scandir "Documentation"
+  - pre kazdy .md vytvorit ekvivalent .html v peknom dizajne
+  - zistit, ako pouzivat relativne URL v .md
+*/
+
 file_put_contents(__DIR__ . '/../html/index.html', 'Toto je akoze staticka stranka. ' . date('Y-m-d H:i:s'));
